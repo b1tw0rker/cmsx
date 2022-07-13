0ae1584e5d2289055940cfa72136b07d7abb6dc4
bit-worker2019 <56485826+bit-worker2019@users.noreply.github.com>
2022-07-13 09:56:11 +0200

    test

--- src/Hello.php
+++ src/Hello.php
@@ -11,10 +11,71 @@
 // namespace HelloComposer;
 namespace Bitworker\Cmsx;
 
-class Hello
+/* class Hello
 {
     public function say($toSay = "Nothing given")
     {
         return $toSay;
+    }
+}
+ */
+
+
+class Hello
+{
+    var $usrid = "12345";
+
+    function Hello()
+    {
+        # dieses "function" heisst wie die Klasse - sobald die
+        # Klasse mit NEW erstellt wird, wird diese function
+        # als erstes aufgerufen - ohne dass wir sie manuell
+        # starten
+    }
+
+    function schreibehallo()
+    {
+        echo "Hallo<br>";
+    }
+
+    function schreibetext($text)
+    {
+        echo $text . "<br>";
+    }
+
+    function schreibetestvariable()
+    {
+        echo $this->usrid . "<br>";
+    }
+
+    /**
+     * 
+     * do logging
+     * 
+     * use $this->filelog === true logging to error-log only. For testing purpose.
+     * 
+     */
+    function log($usrid, $action, $state)
+    {
+        global $db;
+
+        if ($this->filelog === true) {
+
+            error_log($usrid . " - " . $action . " - " . $state . " - " . $this->filelog);
+        } else {
+
+            /*  $db->begin_Transaction();
+            try { */
+            $stmt = $db->prepare('INSERT INTO  ' . TBL_LOGS . ' (usrid, action, state, ip, browser, time) values (?, ?, ?, ?, ?, ?)');
+            $stmt->bind_param('issssi', $usrid, $action, $state, $_SERVER['REMOTE_ADDR'], $_SERVER['HTTP_USER_AGENT'], time());
+            $stmt->execute();
+            $stmt->close();
+            // Datenbank-Transaktion durchführen
+            /*  $db->commit();
+            } catch (Error $e) {
+                $db->rollback();
+                error_log($e);
+            } */
+        }
     }
 }
